596efede3d51bdffb87918b46d31b607b3a89030
Ly Nguyen <ldnguyen58@gmail.com>
2016-02-22 15:17:44 -0800

    Building data model with Mongo and Mongoose

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 /* GET 'about' page */
 module.exports.about = function(rep, res) {
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', {
+    title: 'About Loc8r',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLoc8r se giup ban tim dia diem lam viec thuan tien co ho tro wifi.'
+  });
 };
