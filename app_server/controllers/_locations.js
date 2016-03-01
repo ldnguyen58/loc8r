031f400b3359675c42a7d9be9030feba1e53cf6e
Ly Nguyen <ldnguyen58@gmail.com>
2016-03-01 11:50:34 -0800

    Adding Angular components to the Express application

--- app_server/controllers/_locations.js
+++ app_server/controllers/_locations.js
@@ -6,6 +6,15 @@ if (process.env.NODE_ENV === 'production') {
 
 /* GET 'home' page */
 var renderHomepage = function(req, res, responseBody) {
+  var message;
+  if (!(responseBody instanceof Array)) {
+    message = "API lookup error";
+    responseBody = [];
+  } else {
+    if (!responseBody.length) {
+      message = "No places found nearby";
+    }
+  }
   res.render('locations-list', {
     title: 'Loc8r - find a place to work with wifi',
     pageHeader: {
@@ -13,6 +22,8 @@ var renderHomepage = function(req, res, responseBody) {
       strapline: 'Find places to work with wifi near you!'
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    locations: responseBody,
+    message: message
   });
 };
 var _formatDistance = function(distance) {
@@ -27,7 +38,24 @@ var _formatDistance = function(distance) {
   return numDistance + unit;
 };
 module.exports.homelist = function(req, res) {
-  renderHomepage(req, res);
+  var requestOptions, path;
+  path = '/api/locations';
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: "GET",
+    json: {},
+    qs: {lng: -117.792105, lat: 33.726291, maxDistance: 20}
+  };
+  request(requestOptions, function(err, response, body) {
+    var i, data;
+    data = body;
+    if (response.statusCode === 200 && data.length) {
+      for (i = 0; i < data.length; i++) {
+        data[i].distance = _formatDistance(data[i].distance);
+      }
+    }
+    renderHomepage(req, res, body);
+  });
 };
 
 /* GET 'Location info' page */
